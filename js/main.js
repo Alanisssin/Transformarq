5461f3ecd4156b2634ca59ddef8bd616d79448f6
alanisssin <alanisssin@gmail.com>
2023-01-28 16:15:15 +0100

    tercer commit

--- js/main.js
+++ js/main.js
@@ -16,3 +16,29 @@ $(function () {
         alert("mail sent successfully")
       });
   }
+
+const menuButton = document.querySelector(".mobile-menu");
+const navMenu = document.querySelector(".nav-menu");
+
+let menuOpen = false;
+
+menuButton.addEventListener("click", () => {
+	if (!menuOpen) {
+		navMenu.classList.add("open");
+		menuButton.classList.add("open");
+		menuOpen = true;
+	} else {
+		navMenu.classList.remove("open");
+		menuButton.classList.remove("open");
+		menuOpen = false;
+	}
+});
+
+
+const newYear = document.getElementById("date");
+
+const updateYear = () => {
+    newYear.textContent = new Date().getFullYear();
+};
+
+document.addEventListener("DOMContentLoaded", updateYear);
